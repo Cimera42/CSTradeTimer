1ce4d832895af072bedcab55a38f3fd8cd6f332e
Jono Green <aeirsoul@gmail.com>
2016-01-09 17:45:00 +1100

    Added update prices automatically and ability to catch web based errors

--- Program.cs
+++ Program.cs
@@ -17,11 +17,14 @@ namespace CSTradeTimer
 
 		public static void Main (string[] args)
 		{
-			string password = System.IO.File.ReadAllText(@"E:\Programming\Web\cstrade_admin_password.txt");
+			string password = System.IO.File.ReadAllText(@"F:\Stuff\Websites\cstrade_admin_password.txt");
 			var passworddata = Encoding.ASCII.GetBytes ("password=" + password);
+			var updatePricePer = 240; //Update prices every X pots (currently every 2 hours (30sec pots))
+			var updatePriceCount = 239; //Update at start of program
 			while(true)
 			{
-				string startUrl = "http://10.0.0.53:4001/backend/new_pot.php";
+				updatePriceCount++;
+				string startUrl = "http://127.0.0.1:7001/backend/new_pot.php";
 				HttpWebRequest startRequest = (HttpWebRequest) WebRequest.Create (startUrl);
 
 				startRequest.Method = "POST";
@@ -32,57 +35,98 @@ namespace CSTradeTimer
 					stream.Write (passworddata, 0, passworddata.Length);
 				}
 
-				HttpWebResponse startResponse = (HttpWebResponse) startRequest.GetResponse ();
-				string startString = new StreamReader (startResponse.GetResponseStream()).ReadToEnd();
-				Console.WriteLine("str: " + startString);
-				TimerData data = JsonConvert.DeserializeObject<TimerData> (startString);
-				Console.WriteLine("Timer started: " + data.timer_start + " - " + data.timer_length);
-
-				int timerLength = data.timer_length * 1000;
-				int sleepTime = timerLength*3/4;
-				Console.WriteLine("Timer sleep " + sleepTime);
-				Thread.Sleep(sleepTime);
-				Console.WriteLine("Timer sleep ended");
-
-				long timerEnd = data.timer_start + timerLength;
-				TimeSpan now = (DateTime.UtcNow - new DateTime(1970, 1, 1));
-				while(now.TotalMilliseconds < timerEnd)
+				TimerData data = new TimerData();
+				bool startSucceeded = false;
+				for(int attemptNum = 0; attemptNum < 10; attemptNum++)
 				{
-					now = (DateTime.UtcNow - new DateTime(1970, 1, 1));
+					try {
+						HttpWebResponse startResponse = (HttpWebResponse)startRequest.GetResponse ();
+						string startString = new StreamReader (startResponse.GetResponseStream ()).ReadToEnd ();
+						Console.WriteLine ("str: " + startString);
+						data = JsonConvert.DeserializeObject<TimerData> (startString);
+						Console.WriteLine ("Timer started: " + data.timer_start + " - " + data.timer_length);
+						startSucceeded = true;
+						break;
+					} catch (Exception e) {
+						Console.WriteLine (e.Message);
+					}
 				}
-				Console.WriteLine("Timer ended");
 
-				string endUrl = "http://10.0.0.53:4001/backend/end_timer.php";
-				HttpWebRequest endRequest = (HttpWebRequest) WebRequest.Create (endUrl);
+				if (startSucceeded) {
+					int timerLength = data.timer_length * 1000;
+					int sleepTime = timerLength * 3 / 4;
+					Console.WriteLine ("Timer sleep " + sleepTime);
+					Thread.Sleep (sleepTime);
+					Console.WriteLine ("Timer sleep ended");
 
-				endRequest.Method = "POST";
-				endRequest.ContentType = "application/x-www-form-urlencoded";
-				endRequest.ContentLength = passworddata.Length;
+					long timerEnd = data.timer_start + timerLength;
+					TimeSpan now = (DateTime.UtcNow - new DateTime (1970, 1, 1));
+					while (now.TotalMilliseconds < timerEnd) {
+						now = (DateTime.UtcNow - new DateTime (1970, 1, 1));
+					}
+					Console.WriteLine ("Timer ended");
 
-				using (var stream = endRequest.GetRequestStream ()) {
-					stream.Write (passworddata, 0, passworddata.Length);
+					string endUrl = "http://127.0.0.1:7001/backend/end_timer.php";
+					HttpWebRequest endRequest = (HttpWebRequest)WebRequest.Create (endUrl);
+
+					endRequest.Method = "POST";
+					endRequest.ContentType = "application/x-www-form-urlencoded";
+					endRequest.ContentLength = passworddata.Length;
+
+					using (var stream = endRequest.GetRequestStream ()) {
+						stream.Write (passworddata, 0, passworddata.Length);
+					}
+
+					HttpWebResponse endResponse = (HttpWebResponse)endRequest.GetResponse ();
+					string endString = new StreamReader (endResponse.GetResponseStream ()).ReadToEnd ();
+					Console.WriteLine ("estr: " + endString);
+					Console.WriteLine ("Timer end message sent");
+
+					string processUrl = "http://127.0.0.1:7001/backend/choose_winner.php";
+					HttpWebRequest processRequest = (HttpWebRequest)WebRequest.Create (processUrl);
+
+					processRequest.Method = "POST";
+					processRequest.ContentType = "application/x-www-form-urlencoded";
+					processRequest.ContentLength = passworddata.Length;
+
+					using (var stream = processRequest.GetRequestStream ()) {
+						stream.Write (passworddata, 0, passworddata.Length);
+					}
+
+					bool processSucceeded = false;
+					for (int attemptNum = 0; attemptNum < 10; attemptNum++) {
+						try {
+							HttpWebResponse processResponse = (HttpWebResponse)processRequest.GetResponse ();
+							string processString = new StreamReader (processResponse.GetResponseStream ()).ReadToEnd ();
+
+							Console.WriteLine ("pstr: " + processString);
+							if(processString.Contains("success"))
+							{
+								Console.WriteLine ("Pot processed");
+								processSucceeded = true;
+								break;
+							}
+						} catch (Exception e) {
+							Console.WriteLine (e.Message);
+						}
+					}
+					if (processSucceeded == false) {
+						Console.WriteLine ("####################");
+						Console.WriteLine ("CATASTROPHIC FAILURE");
+						Console.WriteLine ("####################");
+					}
+
+					Thread.Sleep (3000);
+
+					if (updatePriceCount == updatePricePer) {
+						updatePriceCount = 0;
+
+						System.Diagnostics.Process proc = new System.Diagnostics.Process ();
+						proc.StartInfo.FileName = "F:\\Stuff\\Websites\\CSTrade\\trunk\\updatePrices.bat";
+						proc.StartInfo.WorkingDirectory = "F:\\Stuff\\Websites\\CSTrade\\trunk\\";
+						proc.Start ();
+					}
 				}
-				HttpWebResponse endResponse = (HttpWebResponse) endRequest.GetResponse ();
-				string endString = new StreamReader (endResponse.GetResponseStream()).ReadToEnd();
-				Console.WriteLine("estr: " + endString);
-				Console.WriteLine("Timer end message sent");
-
-				string processUrl = "http://10.0.0.53:4001/backend/choose_winner.php";
-				HttpWebRequest processRequest = (HttpWebRequest) WebRequest.Create (processUrl);
-
-				processRequest.Method = "POST";
-				processRequest.ContentType = "application/x-www-form-urlencoded";
-				processRequest.ContentLength = passworddata.Length;
-
-				using (var stream = processRequest.GetRequestStream ()) {
-					stream.Write (passworddata, 0, passworddata.Length);
-				}
-				HttpWebResponse processResponse = (HttpWebResponse) processRequest.GetResponse ();
-				string processString = new StreamReader (processResponse.GetResponseStream()).ReadToEnd();
-				Console.WriteLine("pstr: " + processString);
-				Console.WriteLine("Pot processed");
-
-				Thread.Sleep(3000);
 			}
 		}
 	}
